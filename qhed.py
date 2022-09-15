6ff1a18a2cfc7a72431298b6f66c86b07789aef1
Denny Hwang <sungjoo420@hotmail.com>
2022-09-16 06:32:53 +0900

    QHED_qasm function added

--- qhed.py
+++ qhed.py
@@ -5,14 +5,14 @@ from qiskit.compiler import transpile, assemble
 from qiskit.tools.jupyter import *
 from qiskit.visualization import *
 
-from basicFunctions import amplitude_encode
+from basicFunctions import amplitude_encode, plot_image
 
 import numpy as np
 
 from matplotlib import style
 style.use('bmh')
 
-def QHED_MN(image, thr_ratio=0.7, circuit_display=True, plot_ED_result=True):
+def QHED(image, thr_ratio=0.7, circuit_display=True, plot_ED_result=True):
     '''
 
     '''
@@ -97,6 +97,101 @@ def QHED_MN(image, thr_ratio=0.7, circuit_display=True, plot_ED_result=True):
 
             plot_image(image, 'Original image')
             plot_image(edge_detected_sim, 'Edge Detected image')
+
+        return edge_detected_sim
+
+
+def QHED_qasm(image, thr_ratio=0.7, shots=10000, circuit_display=True, plot_ed_result=True):
+    '''
+
+    '''
+
+    width_qb = np.int(np.ceil(np.log2(image.shape[0])))
+
+    ## 1) Data encoding
+    # Horizontal: Original image
+    image_norm_h = amplitude_encode(image)
+    # Vertical: Transpose of Original image
+    image_norm_v = amplitude_encode(image.T)
+
+    if len(np.unique(image_norm_h)) == 1:
+        return image
+
+    else:
+        ## 2) Generate quantum circuit
+        # Initialize the number of qubits
+        data_qb = width_qb * 2
+        anc_qb = 1
+        total_qb = data_qb + anc_qb
+
+        # Initialize the amplitude permutation unitary
+        D2n_1 = np.roll(np.identity(2 ** total_qb), 1, axis=1)
+
+        # Create the circuit for horizontal scan
+        qc_h = QuantumCircuit(QuantumRegister(1, 'ancila'), QuantumRegister(data_qb, 'q'))
+        qc_h.initialize(image_norm_h, range(1, total_qb))
+        qc_h.barrier()
+        qc_h.h(0)
+        qc_h.unitary(D2n_1, range(total_qb))
+        qc_h.h(0)
+        qc_h.measure_all()
+        if circuit_display:
+            display(qc_h.draw('mpl', fold=-1))
+
+        # Create the circuit for vertical scan
+        qc_v = QuantumCircuit(QuantumRegister(1, 'ancila'), QuantumRegister(data_qb, 'q'))
+        qc_v.initialize(image_norm_v, range(1, total_qb))
+        qc_v.barrier()
+        qc_v.h(0)
+        qc_v.unitary(D2n_1, range(total_qb))
+        qc_v.h(0)
+        qc_v.measure_all()
+        if circuit_display:
+            display(qc_v.draw('mpl', fold=-1))
+
+        # Combine both circuits into a single list
+        circ_list = [qc_h, qc_v]
+
+        ## 3) Measure the state
+        # Simulating the cirucits
+        backend = Aer.get_backend('qasm_simulator')
+
+        # Transpile the circuits for optimized execution on the backend
+        qc_h_t = transpile(qc_h, backend=backend, optimization_level=2)
+        qc_v_t = transpile(qc_v, backend=backend, optimization_level=2)
+        # Combining both circuits into a list
+        circ_list_t = [qc_h_t, qc_v_t]
+
+        # Drawing the transpiled circuit
+        if circuit_display:
+            display(circ_list_t[0].draw('mpl', fold=-1))
+            display(circ_list_t[1].draw('mpl', fold=-1))
+
+        results = backend.run(circ_list_t, shots=shots).result()
+        sim_counts_h = results.get_counts(qc_h_t)
+        sim_counts_v = results.get_counts(qc_v_t)
+
+        # Get the measurement counts from the result
+        sim_keys_h = sim_counts_h.keys()
+        sim_keys_v = sim_counts_v.keys()
+
+        ## 4) Result postprocessing in classical computer
+        # Filter and extract the counts for odd-numbered states
+        # and make the full edge-detected image by adding horizontal and vertical scans
+        edge_scan_sim_h = np.array([sim_counts_h[f'{2 * i + 1:03b}'] if f'{2 * i + 1:03b}' in sim_keys_h else 0 for i in
+                                    range(2 ** data_qb)]).reshape(2 ** width_qb, 2 ** width_qb)
+        edge_scan_sim_v = np.array([sim_counts_v[f'{2 * i + 1:03b}'] if f'{2 * i + 1:03b}' in sim_keys_v else 0 for i in
+                                    range(2 ** data_qb)]).reshape(2 ** width_qb, 2 ** width_qb).T
+        edge_detected_sim = edge_scan_sim_h + edge_scan_sim_v
+
+        if plot_ed_result:
+            # Plotting the Horizontal and vertical scans
+            plot_image(edge_scan_sim_h, 'Horizontal scan output')
+            plot_image(edge_scan_sim_v, 'Vertical scan output')
+
+            # Plotting the original and edge-detected images
+            plot_image(image, 'Original image')
+            plot_image(edge_detected_sim, 'Edge Detected image(QASM)')
 
         return edge_detected_sim
 
